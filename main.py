17cbdca9756558dca8041b3a8eed092f201a3d89
Sigma <nekitbur3868@gmail.com>
2026-09-17 15:33:47 +0300

    fix gitignore filename and clear index

--- main.py
+++
@@ -1,6 +0,0 @@
-from torchvision import transforms
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-])
-
--- tests/tests_dataset.py
+++
@@ -1,6 +0,0 @@
-from src.dataset import load_image
-
-def test_load_image():
-    image = load_image("data/example.jpg")
-
-    assert image is not None
